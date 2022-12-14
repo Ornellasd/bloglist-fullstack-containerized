543e884eb4e19c3ae1cc51e09f33e79fa89ad139
David Ornellas <17051351+Ornellasd@users.noreply.github.com>
2022-12-14 14:46:29 +0100

    clean up add button styling

--- bloglist-frontend/src/components/BlogList.js
+++ bloglist-frontend/src/components/BlogList.js
@@ -25,11 +25,8 @@ const useStyles = makeStyles(() => ({
     lineHeight: 1.35
   },
   addButton: {
-    margin: 0,
-    top: 'auto',
     right: 20,
     bottom: 20,
-    left: 'auto',
     position: 'fixed'
   }
 }))
