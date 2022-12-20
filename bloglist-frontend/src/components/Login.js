1a2a43210dcb1dd050f867fa5dd9b12f630b812d
David Ornellas <17051351+Ornellasd@users.noreply.github.com>
2022-12-20 09:35:18 +0100

    begin merging Login & Signup components

--- bloglist-frontend/src/components/Login.js
+++ bloglist-frontend/src/components/Login.js
@@ -1,8 +1,9 @@
-import React from 'react'
+import React, { useState } from 'react'
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined'
 
 import {
   Avatar,
+  Box,
   Button,
   Container,
   makeStyles,
@@ -12,10 +13,12 @@ import {
 
 import Alerts from './Alerts'
 
-import { useDispatch } from 'react-redux'
-import { Link, useHistory } from 'react-router-dom'
+// import { useDispatch } from 'react-redux'
+// import { Link, useHistory } from 'react-router-dom'
 
-import { login } from '../reducers/loginReducer'
+import { Link } from 'react-router-dom'
+
+// import { login } from '../reducers/loginReducer'
 
 const useStyles = makeStyles(theme => ({
   paper: {
@@ -42,22 +45,141 @@ const useStyles = makeStyles(theme => ({
   }
 }))
 
+const LoginWIP = ({ classes, isLogIn, setIsLogIn }) => {
+  const handleSubmit = () => {
+    console.log('hieperdepiep')
+  }
+
+  if(!isLogIn) {
+    return null
+  }
+
+  return (
+    <form className={classes.form} onSubmit={handleSubmit}>
+      <TextField
+        label="Username"
+        name="username"
+        variant="outlined"
+        margin="normal"
+        required
+        fullWidth
+        autoFocus
+      />
+      <TextField
+        label="Password"
+        type="password"
+        name="password"
+        variant="outlined"
+        margin="normal"
+        required
+        fullWidth
+      />
+      <Button
+        type="submit"
+        fullWidth
+        variant="contained"
+        color="primary"
+        className={classes.submit}
+      >
+        Log In
+      </Button>
+      <Typography>
+        Don&apos;t have an account? Sign Up <Link className={classes.link} onClick={() => setIsLogIn(false)}>Here</Link>
+      </Typography>
+    </form>
+  )
+}
+
+const SignUpWIP = ({ classes, isLogIn, setIsLogIn }) => {
+  const handleSubmit = () => {
+    console.log('hieperdepiep')
+  }
+
+  if(isLogIn) {
+    return null
+  }
+
+  return (
+    <form className={classes.form} onSubmit={handleSubmit}>
+      <Box display="flex" justifyContent="space-between">
+        <TextField
+          label="First Name"
+          name="firstName"
+          variant="outlined"
+          margin="normal"
+          required
+          autoFocus
+          className={classes.nameField}
+        />
+        <TextField
+          label="Last Name"
+          name="lastName"
+          variant="outlined"
+          margin="normal"
+          required
+          className={classes.nameField}
+        />
+      </Box>
+      <TextField
+        label="Username"
+        name="username"
+        variant="outlined"
+        margin="normal"
+        required
+        fullWidth
+      />
+      <TextField
+        label="Password"
+        type="password"
+        name="password"
+        variant="outlined"
+        margin="normal"
+        required
+        fullWidth
+      />
+      <TextField
+        label="Confirm Password"
+        type="password"
+        name="password_confirm"
+        variant="outlined"
+        margin="normal"
+        required
+        fullWidth
+      />
+      <Button
+        type="submit"
+        fullWidth
+        variant="contained"
+        color="primary"
+        className={classes.submit}
+      >
+        Sign Up
+      </Button>
+      <Typography>
+          Have an account? Log In <Link className={classes.link} onClick={() => setIsLogIn(true)}>Here</Link>
+      </Typography>
+    </form>
+  )
+}
+
 const Login = ({ alerts }) => {
-  const dispatch = useDispatch()
-  const history = useHistory()
+  // const dispatch = useDispatch()
+  // const history = useHistory()
   const classes = useStyles()
 
-  const handleSubmit = event => {
-    event.preventDefault()
+  const [isLogIn, setIsLogIn] = useState(true)
 
-    const userCredentials = {
-      username: event.target.username.value,
-      password: event.target.password.value
-    }
+  // const handleSubmit = event => {
+  //   event.preventDefault()
 
-    dispatch(login(userCredentials))
-    history.push('/')
-  }
+  //   const userCredentials = {
+  //     username: event.target.username.value,
+  //     password: event.target.password.value
+  //   }
+
+  //   dispatch(login(userCredentials))
+  //   history.push('/')
+  // }
 
   return (
     <Container maxWidth="sm">
@@ -66,40 +188,10 @@ const Login = ({ alerts }) => {
           <LockOutlinedIcon />
         </Avatar>
         <Typography component="h1" variant="h5">
-          Log In
+          {isLogIn ? 'Log In' : 'Sign Up'}
         </Typography>
-        <form className={classes.form} onSubmit={handleSubmit}>
-          <TextField
-            label="Username"
-            name="username"
-            variant="outlined"
-            margin="normal"
-            required
-            fullWidth
-            autoFocus
-          />
-          <TextField
-            label="Password"
-            type="password"
-            name="password"
-            variant="outlined"
-            margin="normal"
-            required
-            fullWidth
-          />
-          <Button
-            type="submit"
-            fullWidth
-            variant="contained"
-            color="primary"
-            className={classes.submit}
-          >
-            Log In
-          </Button>
-        </form>
-        <Typography>
-          Don&apos;t have an account? Sign Up <Link to="/signup" className={classes.link}>Here</Link>
-        </Typography>
+        <SignUpWIP classes={classes} isLogIn={isLogIn} setIsLogIn={setIsLogIn}/>
+        <LoginWIP classes={classes} isLogIn={isLogIn} setIsLogIn={setIsLogIn} />
         {alerts && <Alerts alerts={alerts} />}
       </div>
     </Container>
